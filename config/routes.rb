46cc62c252bc6a4d3ab2a5adb0907b3c54de3335
Paul Spieker <p.spieker@duenos.de>
2016-04-15 23:29:42 +0200

    fix: support patch and put on confirm

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   namespace :v1 do
     resources :appointments do
       patch :confirm, on: :member, as: :confirm
+      put   :confirm, on: :member
     end
     resources :email_reports, only: %i(create)
     resources :locations
     resources :offers do
       patch :confirm, on: :member, as: :confirm
+      put   :confirm, on: :member
     end
     resources :offer_times
   end
