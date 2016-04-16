f5b2eddbed8ce07b756022222342fda30f5bf24e
Paul Spieker <p.spieker@duenos.de>
2016-04-16 22:42:26 +0200

    fix: relationship filter key

--- app/serializers/v1/offer_serializer.rb
+++ app/serializers/v1/offer_serializer.rb
@@ -4,7 +4,7 @@ module V1
 
     # Relationships
     has_many_link :locations, filter: :offer_locations_offer_id_eq
-    has_many_link :offer_times, filter: :location_id_eq
+    has_many_link :offer_times, filter: :offer_id_eq
 
     # Attributes
     def email
