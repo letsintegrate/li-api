b9fec5e15babcf43d5fafb4975afda9a45fdfea1
Paul Spieker <p.spieker@duenos.de>
2016-05-16 12:15:53 +0200

    fix: nil exception

--- app/validators/email_blacklist_validator.rb
+++ app/validators/email_blacklist_validator.rb
@@ -1,12 +1,12 @@
 class EmailBlacklistValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    domain = value.to_s.downcase.split('@').last.strip
+    domain = value.to_s.downcase.split('@').last.to_s.strip
     if EMAIL_DOMAIN_BLACKLIST.include?(domain)
       record.errors[attribute] << (options[:message] || "blacklisted")
     end
 
     EMAIL_WORD_BLACKLIST.each do |word|
-      if value.include?(word)
+      if value.to_s.include?(word)
         record.errors[attribute] << (options[:message] || "blacklisted")
       end
     end
