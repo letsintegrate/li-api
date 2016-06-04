92a62c22ee54c553d7ff7c9a5e97e1ce328fc1a4
Paul Spieker <p.spieker@duenos.de>
2016-06-04 17:57:19 +0200

    fix: uniqueness validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   # Validations
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, password_strength: {
     use_dictionary: true,
     min_word_length: 8
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe User, type: :model do
   it { should have_db_column(:id).of_type :uuid }
   it { should have_db_column(:email).of_type :string }
   it { should have_db_column(:password_digest).of_type :string }
+
+  # Validations
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of(:email).case_insensitive }
 end
